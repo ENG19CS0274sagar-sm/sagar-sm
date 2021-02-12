cd4795751660a31b8c1d40daaf9d4c0b5d7df3f2
ENG19CS0274sagar-sm <76122953+ENG19CS0274sagar-sm@users.noreply.github.com>
2021-02-12 11:42:36 +0530

    Update tromboloid_with_4_functions.c

--- tromboloid_with_4_functions.c
+++ tromboloid_with_4_functions.c
@@ -1,1 +1,30 @@
 //WAP to find the volume of a tromboloid using 4 functions.
+#include<stdio.h>
+float input()
+{
+  float x;
+  scanf("%f"&x);
+  return x;
+}
+float formulae_trom(float h ,float b , float d)
+{
+  return ((h*b*d)+(d/b))/3;
+}
+
+void print(float vol)
+{
+  printf("volume of tromboloid is =%f ",vol);
+}
+  
+int main()
+{
+  printf("enter b ");
+  float h=input();
+  printf("enter b");
+  float b=input();
+  printf("enter d");
+  float d=input();
+  float vol=formulae_trom(h,b,d);
+  print(vol);
+  return 0;
+}  
